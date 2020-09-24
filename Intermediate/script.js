94f4a1515f9a309c4905f2ac582202e862d20ad8
Christian Bodenwinkler <c.bodenwinkler@gmx.at>
2020-09-24 13:09:27 +0200

    Intermediate Finish

--- Intermediate/script.js
+++ Intermediate/script.js
@@ -1,30 +1,38 @@
-// console.log(employee);
 var dataEmployee = JSON.parse(employee);
 
 console.table(dataEmployee);
 
 function employeeTable(employee) {
-  let cols = dataEmployee.keys(employee[0]);
+  // set variable for columns
+  let cols = Object.keys(employee[0]);
 
-  let headerRow = cols.map((col) => `<th>${col}</th>`).join('');
+  // set variable for headerRow <th>
+  let headerRow = cols
+    .map(cols => `<th> ${cols} </th>`)
+    .join("");
 
+  // set variable for rows <td>
   let rows = employee
-    .map((row) => {
-      let tds = cols.map((col) => `<td>${row[col]}</td>`).join('');
+    .map(row => {
+      let tds = cols.map(cols => `<td>${row[cols]}</td>`).join('');
       return `<tr>${tds}</tr>`;
     })
     .join('');
+
+
   const table = `
-	<table>
-		<thead>
-			<tr>${headerRow}</tr>
-		<thead>
-		<tbody>
-			${rows}
-		<tbody>
-	<table>`;
+    <table>
+      <thead>
+        <tr>${headerRow}</tr>
+      <thead>
+      <tbody>
+        <td> ${rows} </td>
+      <tbody>
+    <table>`;
 
   return table;
 }
+
 output = document.getElementById('result');
 output.innerHTML = employeeTable(dataEmployee);
+
